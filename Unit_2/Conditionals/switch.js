48d490f203d8e423a1d1948c76c5d5bdcda2a489
Nicholas Rose <nick@Nicholass-MacBook-Air.local>
2023-09-28 20:52:46 -0400

    Finished conditionals with switch and ternary.

---
+++ Unit_2/Conditionals/switch.js
@@ -0,0 +1,42 @@
+/* 
+    Switch Conditional Statement
+    - A switch statement executes/runs a block of code depending on different cases.
+    - The switch is used together with a break or a default keyword or both.
+        - These are both optional
+        - Break: keyword that breaks out of the switch block. If a break is omitted/not added the next code block in the switch will run (run as well).
+        - Default: keyword that specifies code to run if no case matches. (Think what our else did in if statements).
+*/
+
+let officeCharacter = "Pam"; // if a break; is omitted, code will run through to the next break;
+
+switch (officeCharacter) {
+    case "Michael":
+        console.log("My mind is going a mile an hour");
+        break; 
+    case "Dwight":
+        console.log("Perfectenschlag");
+        break;
+    case "Jim":
+        console.log("Bears. Beets. Battlestar Galactica");
+        break;
+    default:
+        console.log(`I'm sorry, ${officeCharacter}, but do I know you?`); // Interpolation:here a string is added into another string. In this case, (if you enter pam into the top line) she is not listed so code will run the default and insert Pam's name.  
+}
+
+// We can use backticks `  ` to create string that can use the value of variables!
+// The variables must be surrounded by ${ }, cash money curly boys.
+
+// let num = -40; // did not work
+// let num = 5; // case 2 ran
+let num = -3; // case 1 ran
+
+switch (true) {
+    case (num < 0 && num > -10):
+        console.log('case 1 ran');
+        break;
+    case (num > 0):
+        console.log('case 2 ran');
+        break;
+    default:
+        console.log('did not work');
+}
---
+++ Unit_2/Conditionals/ternary.js
@@ -0,0 +1,45 @@
+/* 
+    Ternary Conditional Statement
+    -Super cool syntax
+    -A shortcut for writing an if/else, or else/if statement
+    - (condition) ? true result : false result;
+    -Ternaries REQUIRE the default/else catch all, has to have both true and false results for condition
+*/
+
+// let num = 6; // Yes! for both
+let num = -1; // Nope! Nada! (This shows that these two examples are the same thing but the ternary is essentially shorthand.)
+
+// Ternary: 
+
+(num > 0) ? console.log('Yes!') : console.log('Nope!');
+
+// Instead of:
+if (num > 0) {
+    console.log('Yes!')
+} else {
+    console.log('Nadda!')
+}
+
+// Ternary with two conditions vs Else If statement
+
+// let x = 1; // results in all goodbyes in each example
+// let x = -5; // all hi console logs
+let x = 0; // all hello console logs
+
+
+//Else if statement
+if (x == 0) {
+    console.log("hello");
+} else if (x < 0) {
+    console.log("hi");
+} else {
+    console.log("goodbye");
+}
+
+// Ternary
+(x == 0) ? console.log('Hello!') : (x < 0) ? console.log("Hi!") : console.log("Goodbye!");
+
+// Other way to write ternary
+(x == 0) ? console.log("Hello! V2") 
+: (x < 0) ? console.log("Hi! V2") 
+: console.log("Goodbye! V2");
